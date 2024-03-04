82b1bceaa8ef4578772b626c5f1e5d6b5d571382
Erin Kelley <erin.kelley01@gmail.com>
2024-03-03 23:31:39 -0700

    Change weather API to require no auth key

---
+++ src/weather.js
@@ -0,0 +1,10 @@
+function findWeatherCode(num, weatherCodes){
+    let weatherDescription = weatherCodes.find(weather => {
+        return weather.code === num
+    })
+
+    return weatherDescription.description
+    
+}
+
+export { findWeatherCode }
